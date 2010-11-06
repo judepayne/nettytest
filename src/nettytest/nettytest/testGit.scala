7ce1292cd9035f53337d91c2b399a7bdda0c78b8
Jude Payne <judepayne@yahoo.co.uk>
2010-11-06 13:16:26 +0000

    inital commit

---
+++ src/nettytest/nettytest/testGit.scala
@@ -0,0 +1,15 @@
+package nettytest.nettytest
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jude
+ * Date: Nov 6, 2010
+ * Time: 1:15:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class testGit {
+
+  //this class does nothing!
+
+}
